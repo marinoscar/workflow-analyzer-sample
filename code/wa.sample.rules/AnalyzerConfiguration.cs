d0821bf5b91214284bcb6e74c1f31b58d1121518
marinoscar <oscar@marin.cr>
2020-08-13 22:09:59 -0500

    Initial commit

---
+++ code/wa.sample.rules/AnalyzerConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UiPath.Studio.Activities.Api;
+using UiPath.Studio.Activities.Api.Analyzer;
+
+namespace wa.sample.rules
+{
+    /// <summary>
+    /// Initialize the analyzer custom rules
+    /// </summary>
+    /// <remarks>
+    /// Install the pacakge UiPath.Activities.Api from the following stream https://www.myget.org/F/workflow
+    /// </remarks>
+    public class AnalyzerConfiguration : IRegisterAnalyzerConfiguration
+    {
+        public void Initialize(IAnalyzerConfigurationService workflowAnalyzerConfigService)
+        {
+            workflowAnalyzerConfigService.AddRule(SampleRule.Get());
+        }
+    }
+}
